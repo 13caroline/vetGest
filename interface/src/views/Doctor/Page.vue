64187f030cdc75e759768cf13212326b02e13065
João Mota <joaodiogomota12@gmail.com>
2021-05-27 20:25:10 +0100

    Correçao erro

--- interface/src/views/Doctor/Page.vue
+++ interface/src/views/Doctor/Page.vue
@@ -130,7 +130,7 @@
 </template>
 
 <script>
-import Header from "@/components/Headers/ClinicHeader.vue";
+import Header from "@/components/Headers/DoctorHeader.vue";
 import Footer from "@/components/Footer.vue";
 
 export default {
