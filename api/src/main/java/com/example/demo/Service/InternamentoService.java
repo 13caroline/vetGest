c8e1cd079390c8d240ff9ef08f1c0837d47c1275
Teixeira992 <a84430@alunos.uminho.pt>
2021-06-09 18:42:14 +0100

    internamento rotas

--- api/src/main/java/com/example/demo/Service/InternamentoService.java
+++ api/src/main/java/com/example/demo/Service/InternamentoService.java
@@ -1,7 +1,9 @@
 package com.example.demo.Service;
 
 import com.example.demo.Entity.Internamento;
+import com.example.demo.Entity.NotaInternamento;
 import com.example.demo.Repository.InternamentoRepository;
+import com.example.demo.Repository.NotaInternamentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +14,38 @@ public class InternamentoService {
     @Autowired
     private InternamentoRepository repository;
 
+    @Autowired
+    private NotaInternamentoRepository repositoryNota;
+
     public List<Internamento> getAllInternamentosByVetId(int veterinario_id) {
         return repository.findAllByVeterinarioId(veterinario_id);
     }
 
-
     public List<Internamento> getInternamentoByAnimalId(int animal_id) {
         return repository.findAllByAnimalId(animal_id);
+    }
+
+    public List<Internamento> findAllByVeterinarioEmailAndAnimalId(String vet_email, int animal_id){
+        return repository.findAllByVeterinarioEmailAndAnimalId(vet_email, animal_id);
+    }
+
+    public Internamento saveInternamento(Internamento internamento){
+        return repository.save(internamento);
+    }
+
+    public List<Internamento> findAllByVeterinarioEmailAndIdAndAnimalId(String vet_email, int internamento_id ,int animal_id){
+        return repository.findAllByVeterinarioEmailAndIdAndAnimalId(vet_email,internamento_id,animal_id);
+    }
+
+    public List<NotaInternamento> findAllByInternamento(Internamento internamento){
+        return repositoryNota.findAllByInternamento(internamento);
+    }
+
+    public Internamento findById(int internamento){
+        return repository.findById(internamento);
+    }
+
+    public NotaInternamento saveNota(NotaInternamento nota){
+        return repositoryNota.save(nota);
     }
 }
