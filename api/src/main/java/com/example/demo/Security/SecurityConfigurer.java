8032b7298150d0eed79dd341330a206483330c0f
Teixeira992 <a84430@alunos.uminho.pt>
2021-06-10 16:12:31 +0100

    segurança rotas

--- api/src/main/java/com/example/demo/Security/SecurityConfigurer.java
+++ api/src/main/java/com/example/demo/Security/SecurityConfigurer.java
@@ -67,25 +67,14 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.GET).permitAll()
-                .antMatchers(HttpMethod.POST).permitAll()
-                .antMatchers(HttpMethod.PUT).permitAll()
-                .antMatchers("/authenticate","/cliente/registar","/clinica/adicionarClinica").permitAll()
-                .antMatchers("/addDica/{emailVet}","/medico/intervencoes","/medico/agendar/intervencao","/medico/utentes",
-                        "/medico/cirurgias","/medico/utente","/medico/animal/adiciona/imunizacao","/medico/utente/vacinas",
-                        "/medico/utente/desparasitacoes","/medico/animal/confirma/imunizacao","/medico/utente/editar","/medico/internamento",
-                        "/medico/internamento/detalhes").hasAnyRole("Veterinario")
+                .antMatchers("/","/authenticate","/cliente/registar","/clinica/adicionarClinica").permitAll()
+                .antMatchers("/medico/*").hasAnyRole("Veterinario")
                 //Acessos da Clinica
-                .antMatchers("/addVet","/clinica/consultas","/clinica/clientes",
-                        "/clinica/utentes/registar","/clinica/clientes/registar","/clinica/medicos","/clinica/medicos/registar","/clinica/utentes",
-                        "/clinica/intervencao/alterar","/clinica/utente","/clinica/imunizacao","/clinica/intervencao").hasAnyRole("Clinica")
+                .antMatchers("/clinica/*").hasAnyRole("Clinica")
                 //Acessos do Cliente
-                .antMatchers("/","/cliente","/cliente/animal/registar","/cliente/animal/{id_animal}",
-                        "/cliente/animal/cancelar/{id_intervencao}","/cliente/animal/{id_animal}","/cliente/animal/{id_animal}/getvacinas",
-                        "/cliente/consulta","/cliente/consultas","/cliente/cirurgias","/cliente/preferenciassa","/sobre-nos","/dicas",
-                        "/cliente/animal/confirma/imunizacao").hasAnyRole("Cliente")
+                .antMatchers("/cliente/*").hasAnyRole("Cliente")
                 //Acessos de todos
-                .antMatchers("/","/terminar-sessao").hasAnyRole("Clinica","Veterinario","Cliente").
+                .antMatchers("/terminar-sessao").hasAnyRole("Clinica","Veterinario","Cliente").
                 anyRequest().authenticated().and().
                 exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
