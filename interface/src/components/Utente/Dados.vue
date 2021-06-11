5b51b5ef5cf3c97903c5a79896c4fa8b4ed4c064
Valeriy Apostolyuk <valeriy_87@hotmail.com>
2021-06-11 20:38:43 +0100

    Updated dados layout.

--- interface/src/components/Utente/Dados.vue
+++ interface/src/components/Utente/Dados.vue
@@ -1,228 +1,214 @@
 <template>
-  <div>
-    <v-card flat color="#fafafa">
-      <v-row>
-        <v-col cols="12">
-          <v-row align="center" class="my-3">
-            <v-col>
-              <h3 class="font-weight-regular text-uppercase">
-                <v-icon small>fas fa-paw</v-icon>
-                Informações Gerais
-              </h3>
-            </v-col>
-            <v-col cols="auto" class="ml-auto pl-0">
-              <v-tooltip top>
-                <template v-slot:activator="{ on, attrs }">
-                  <v-btn
-                    class="body-2 ml-2 mb-2"
-                    small
-                    color="#2596be"
-                    v-bind="attrs"
-                    v-on="on"
-                    fab
-                    dark
-                    @click="editar()"
-                  >
-                    <v-icon small>fas fa-pen</v-icon>
-                  </v-btn>
-                </template>
-                <span>Editar dados</span>
-              </v-tooltip>
-            </v-col>
-          </v-row>
+  <div class="bg-color">
+    <v-row align="center" class="py-3">
+      <v-col>
+        <h3 class="font-weight-regular text-uppercase">
+          <v-icon small>fas fa-paw</v-icon>
+          Informações Gerais
+        </h3>
+      </v-col>
+      <v-col cols="auto" class="ml-auto pl-0">
+        <v-tooltip top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              class="body-2 ma-2"
+              small
+              color="#2596be"
+              v-bind="attrs"
+              v-on="on"
+              fab
+              dark
+              @click="editar()"
+            >
+              <v-icon small>fas fa-pen</v-icon>
+            </v-btn>
+          </template>
+          <span>Editar dados</span>
+        </v-tooltip>
+      </v-col>
+    </v-row>
 
-          <h3 class="mb-3">Identificação</h3>
-          <v-divider></v-divider>
+    <h3>Identificação</h3>
+    <v-divider class="my-3"></v-divider>
 
-          <v-row class="w-100" align="stretch">
-            <v-col>
-              <v-card class="h-100" outlined>
-                <v-list-item>
-                  <v-list-item-content>
-                    <div>
-                      <v-row>
-                        <v-col>
-                          <p class="infos">Nome</p>
-                        </v-col>
-                        <v-col>
-                          <p class="respos">{{ animal.nome }}</p>
-                        </v-col>
-                      </v-row>
-                    </div>
-                    <div>
-                      <v-row>
-                        <v-col>
-                          <p class="infos">Data de Nascimento</p>
-                        </v-col>
-                        <v-col>
-                          <p class="respos">
-                            {{ format(animal.dataNascimento) }}
-                          </p>
-                        </v-col>
-                      </v-row>
-                    </div>
-                    <div>
-                      <v-row>
-                        <v-col>
-                          <p class="infos">Sexo</p>
-                        </v-col>
-                        <v-col>
-                          <p class="respos">{{ animal.sexo }}</p>
-                        </v-col>
-                      </v-row>
-                    </div>
-                    <div>
-                      <v-row>
-                        <v-col>
-                          <p class="infos">Chip</p>
-                        </v-col>
-                        <v-col>
-                          <p class="respos">{{ animal.chip }}</p>
-                        </v-col>
-                      </v-row>
-                    </div>
-                  </v-list-item-content>
-                </v-list-item>
-              </v-card>
-            </v-col>
-            <v-col>
-              <v-card class="h-100" outlined>
-                <v-list-item>
-                  <v-list-item-content>
-                    <div>
-                      <v-row>
-                        <v-col>
-                          <p class="infos">Espécie</p>
-                        </v-col>
-                        <v-col>
-                          <p class="respos">{{ animal.especie }}</p>
-                        </v-col>
-                      </v-row>
-                    </div>
-                    <div>
-                      <v-row>
-                        <v-col>
-                          <p class="infos">Raça</p>
-                        </v-col>
-                        <v-col>
-                          <p class="respos">{{ animal.raca }}</p>
-                        </v-col>
-                      </v-row>
-                    </div>
-                    <div>
-                      <v-row>
-                        <v-col>
-                          <p class="infos">Esterilizado</p>
-                        </v-col>
-                        <v-col>
-                          <p v-if="animal.castracao === true" class="respos">
-                            Sim
-                          </p>
-                          <p v-else class="respos">Não</p>
-                        </v-col>
-                      </v-row>
-                    </div>
-                    <div>
-                      <v-row>
-                        <v-col>
-                          <p class="infos">Dono</p>
-                        </v-col>
-                        <v-col>
-                          <p class="respos">{{ animal.cliente_nome }}</p>
-                        </v-col>
-                      </v-row>
-                    </div>
-                  </v-list-item-content>
-                </v-list-item>
-              </v-card>
-            </v-col>
-
-            <v-col>
-              <div class="foto">
-                <v-card flat>
-                  <v-img
-                    src="@/assets/animais/Rubi.jpg"
-                    aspect-ratio="1"
-                    class="grey lighten-2 mx-2"
-                    cover
-                  >
-                    <template v-slot:placeholder>
-                      <v-row
-                        class="fill-height ma-0"
-                        align="center"
-                        justify="center"
-                      >
-                        <v-progress-circular
-                          indeterminate
-                          color="grey lighten-5"
-                        ></v-progress-circular>
-                      </v-row>
-                    </template>
-                  </v-img>
-                </v-card>
+    <v-row class="w-100" align="stretch">
+      <v-col>
+        <v-card class="h-100" outlined>
+          <v-list-item>
+            <v-list-item-content>
+              <div>
+                <v-row>
+                  <v-col>
+                    <p class="infos">Nome</p>
+                  </v-col>
+                  <v-col>
+                    <p class="respos">{{ animal.nome }}</p>
+                  </v-col>
+                </v-row>
               </div>
-            </v-col>
-          </v-row>
+              <div>
+                <v-row>
+                  <v-col>
+                    <p class="infos">Data de Nascimento</p>
+                  </v-col>
+                  <v-col>
+                    <p class="respos">
+                      {{ format(animal.dataNascimento) }}
+                    </p>
+                  </v-col>
+                </v-row>
+              </div>
+              <div>
+                <v-row>
+                  <v-col>
+                    <p class="infos">Sexo</p>
+                  </v-col>
+                  <v-col>
+                    <p class="respos">{{ animal.sexo }}</p>
+                  </v-col>
+                </v-row>
+              </div>
+              <div>
+                <v-row>
+                  <v-col>
+                    <p class="infos mb-0">Chip</p>
+                  </v-col>
+                  <v-col>
+                    <p class="respos mb-0">{{ animal.chip }}</p>
+                  </v-col>
+                </v-row>
+              </div>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card class="h-100" outlined>
+          <v-list-item>
+            <v-list-item-content>
+              <div>
+                <v-row>
+                  <v-col>
+                    <p class="infos">Espécie</p>
+                  </v-col>
+                  <v-col>
+                    <p class="respos">{{ animal.especie }}</p>
+                  </v-col>
+                </v-row>
+              </div>
+              <div>
+                <v-row>
+                  <v-col>
+                    <p class="infos">Raça</p>
+                  </v-col>
+                  <v-col>
+                    <p class="respos">{{ animal.raca }}</p>
+                  </v-col>
+                </v-row>
+              </div>
+              <div>
+                <v-row>
+                  <v-col>
+                    <p class="infos">Esterilizado</p>
+                  </v-col>
+                  <v-col>
+                    <p v-if="animal.castracao === true" class="respos">Sim</p>
+                    <p v-else class="respos">Não</p>
+                  </v-col>
+                </v-row>
+              </div>
+              <div>
+                <v-row>
+                  <v-col>
+                    <p class="infos mb-0">Dono</p>
+                  </v-col>
+                  <v-col>
+                    <p class="respos mb-0">{{ animal.cliente_nome }}</p>
+                  </v-col>
+                </v-row>
+              </div>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+      </v-col>
 
-          <h3 class="mt-6 mb-3">Características</h3>
-          <v-divider></v-divider>
+      <v-col cols="auto">
+        <div class="foto">
+          <v-img
+            src="@/assets/animais/Rubi.jpg"
+            aspect-ratio="1"
+            class="grey lighten-2 mx-2"
+            cover
+          >
+            <template v-slot:placeholder>
+              <v-row class="fill-height ma-0" align="center" justify="center">
+                <v-progress-circular
+                  indeterminate
+                  color="grey lighten-5"
+                ></v-progress-circular>
+              </v-row>
+            </template>
+          </v-img>
+        </div>
+      </v-col>
+    </v-row>
 
-          <v-row class="w-100" align="stretch">
-            <v-col>
-              <v-card class="h-100" outlined>
-                <v-list-item>
-                  <v-list-item-content>
-                    <div>
-                      <v-row>
-                        <v-col>
-                          <p class="infos">Altura</p>
-                        </v-col>
-                        <v-col>
-                          <p class="respos">{{ animal.altura }} (cm)</p>
-                        </v-col>
-                      </v-row>
-                    </div>
+    <h3 class="mt-6">Características</h3>
+    <v-divider class="my-3"></v-divider>
 
-                    <div>
-                      <v-row>
-                        <v-col>
-                          <p class="infos">Pelagem</p>
-                        </v-col>
-                        <v-col>
-                          <p class="respos">{{ animal.pelagem }}</p>
-                        </v-col>
-                      </v-row>
-                    </div>
+    <v-row class="w-100" align="stretch">
+      <v-col>
+        <v-card class="h-100" outlined>
+          <v-list-item>
+            <v-list-item-content>
+              <div>
+                <v-row>
+                  <v-col>
+                    <p class="infos">Altura</p>
+                  </v-col>
+                  <v-col>
+                    <p class="respos">{{ animal.altura }} (cm)</p>
+                  </v-col>
+                </v-row>
+              </div>
 
-                    <div>
-                      <v-row>
-                        <v-col>
-                          <p class="infos">Cor</p>
-                        </v-col>
-                        <v-col>
-                          <p class="respos">{{ animal.cor }}</p>
-                        </v-col>
-                      </v-row>
-                    </div>
+              <div>
+                <v-row>
+                  <v-col>
+                    <p class="infos">Pelagem</p>
+                  </v-col>
+                  <v-col>
+                    <p class="respos">{{ animal.pelagem }}</p>
+                  </v-col>
+                </v-row>
+              </div>
 
-                    <div>
-                      <v-row>
-                        <v-col>
-                          <p class="infos">Cauda</p>
-                        </v-col>
-                        <v-col>
-                          <p class="respos">{{ animal.cauda }}</p>
-                        </v-col>
-                      </v-row>
-                    </div>
-                  </v-list-item-content>
-                </v-list-item>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-card>
+              <div>
+                <v-row>
+                  <v-col>
+                    <p class="infos">Cor</p>
+                  </v-col>
+                  <v-col>
+                    <p class="respos">{{ animal.cor }}</p>
+                  </v-col>
+                </v-row>
+              </div>
+
+              <div>
+                <v-row>
+                  <v-col>
+                    <p class="infos">Cauda</p>
+                  </v-col>
+                  <v-col>
+                    <p class="respos">{{ animal.cauda }}</p>
+                  </v-col>
+                </v-row>
+              </div>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -274,5 +260,8 @@ export default {
 
 .row.col.col-6 {
   margin-top: 0;
+}
+.bg-color {
+  background: #fafafa;
 }
 </style>
