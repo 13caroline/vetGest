84e21a3e6382bbb271004cf97553a0cf70898ba0
Teixeira992 <a84430@alunos.uminho.pt>
2021-06-14 22:30:52 +0100

    fix horario consultas

--- api/src/main/java/com/example/demo/Service/MailService.java
+++ api/src/main/java/com/example/demo/Service/MailService.java
@@ -54,11 +54,13 @@ public class MailService {
         mailSender.setPassword("projetoEA");
 
         Properties props = mailSender.getJavaMailProperties();
+        System.out.println(props);
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.debug", "true");
-
+        props.put("mail.ssl.enable", "true");
+        props.put("mail.smtp.ssl.trust", "*");
         return mailSender;
     }
 }
