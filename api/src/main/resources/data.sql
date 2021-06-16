1a3fd9e4939e831bf02350d81c803b3009c80c34
Bruno Teixeira <47892672+Teixeira992@users.noreply.github.com>
2021-06-16 01:25:51 +0100

    Delete data.sql

--- api/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-/*insert into utilizador (dtype,nome, email,password,concelho, contacto, freguesia, morada, nif)
-values ('Clinica', 'Clínica Veterinária do Minho', 'clinicaveterinariadominho@gmail.com', '$2a$10$KoDr0ame1kST9Xjw6OhPAu66L5pNpQHO6By5UznaKnDnrNKqxmXV.',
-        'Braga', '253601100', 'Gualtar', 'Rua da Universidade 4710-057', 111111111);
-*/
